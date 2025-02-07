a0e91a40a8cd069a776fc4147af30411d112463f
bjhardcastle <ben.hardcastle@alleninstitute.org>
2025-02-06 19:43:44 -0800

    Clean up plots

--- app_charts.py
+++ app_charts.py
@@ -9,6 +9,7 @@ import param
 import polars as pl
 import upath
 from panel.custom import ReactComponent
+import numpy as np
 import polars.selectors as cs 
 
 import db_utils
@@ -33,13 +34,14 @@ unfiltered_df = (
         how='inner',
     )
 )
+QUANTILE = 0.99
 def filter_outlier_exprs() -> list[pl.Expr]:
     exprs = []
     for column in (
         'drift_ptp', 
         'amplitude_cutoff',
     ):
-        q: float | None = unfiltered_df[column].quantile(0.99, interpolation='lower')
+        q: float | None = unfiltered_df[column].quantile(QUANTILE, interpolation='lower')
         exprs.append(
             (pl.col(column) <= q) | (pl.col(column).is_null()) | (pl.col(column).is_nan()))
     return exprs
@@ -58,7 +60,7 @@ metric_dropdown = alt.binding_select(
     name='metric'
 )
 metric_param = alt.param(
-    value='drift_ptp',
+    value=metric_columns[0],
     bind=metric_dropdown
 )
 
@@ -71,7 +73,14 @@ brush = alt.selection_interval(
 def get_histogram_chart(df: pl.DataFrame, title: str | None = None) -> alt.Chart:
     # alt.Chart(df.to_pandas())
     return (
-        alt.Chart(df.to_pandas())
+        alt.Chart(
+            data=df.to_pandas(),
+            title=alt.Title(
+                title or "",
+                anchor='start',
+                orient='bottom',
+            )
+        )
         .transform_calculate(
             metric=f'datum[{metric_param.name}]'
         )
@@ -91,14 +100,18 @@ def get_histogram_chart(df: pl.DataFrame, title: str | None = None) -> alt.Chart
             metric_param,
             brush,
         )
-        .properties(
-            title=title or "",
-        )
     ).interactive()
 
-def get_cum_dist_chart(df) -> alt.Chart:
+def get_cum_dist_chart(df: pl.DataFrame) -> alt.Chart:
     return (
-        alt.Chart(df.to_pandas())
+        alt.Chart(
+            data=df.to_pandas(),
+            title=alt.Title(
+                '(nan values may affect starting point)',
+                anchor='start',
+                orient='bottom',
+            )
+        )
         .transform_calculate(
             metric=f'datum[{metric_param.name}]'
         )
@@ -107,13 +120,14 @@ def get_cum_dist_chart(df) -> alt.Chart:
             frame=[-5, 5],
             sort=[{"field": "metric"}],
         )
-        .mark_line(
-            interpolate="step-after"
-        )
         .encode(
             alt.X('metric:Q', title=''),
             alt.Y("cdf:Q"),
             alt.Color('drift_rating:N'),
+            tooltip=['drift_rating:N', 'metric:Q', 'cdf:Q'],
+        )
+        .mark_line(
+            interpolate="step-after"
         )
         .add_params(
             metric_param,
@@ -123,10 +137,9 @@ def get_cum_dist_chart(df) -> alt.Chart:
 
 outliers_df = unfiltered_df.join(filtered_df, on='unit_id', how='anti')  
 metric_charts =  pn.pane.Vega((
-    (get_histogram_chart(filtered_df) | get_cum_dist_chart(filtered_df)) )
-    & (get_histogram_chart(outliers_df, title='outliers (in any metric)') | get_cum_dist_chart(outliers_df))
-                                
-) 
+    (get_histogram_chart(filtered_df, title="excluding outliers") | get_cum_dist_chart(filtered_df)) )
+    & (get_histogram_chart(outliers_df, title=f'outliers (>{100*QUANTILE}th percentile of any metric)') | get_cum_dist_chart(outliers_df))                               
+)
 
 df = db_utils.get_df()
 stats = pn.pane.DataFrame(
@@ -142,14 +155,45 @@ stats = pn.pane.DataFrame(
     index=False,
 )
 
+timeline = pn.pane.Vega(
+    alt.Chart(
+        data=(
+            db_utils.get_df()
+            .drop_nulls('drift_rating')
+            .with_columns(
+                pl.from_epoch('checked_timestamp').alias('timestamp'),
+            )
+            .drop('session_id', 'drift_rating', 'checked_timestamp')
+            .sort('timestamp')
+            .group_by_dynamic(
+                'timestamp',
+                every="1h",
+            )
+            .agg(pl.col('unit_id').count().alias('annotations'))
+        ).to_pandas(),
+    )
+    .mark_rect()
+    .encode(
+        alt.X('hoursminutes(timestamp):O').title('hour'),
+        alt.Y('monthdate(timestamp):O').title('date'),
+        alt.Color('annotations:Q', legend=None).scale(scheme="greens"),
+        tooltip=['annotations:Q'],
+    )
+    .properties(
+        title='annotations per hour',
+    )
+    .interactive()
+)
+
 def app():
     return pn.template.MaterialTemplate(
         site="DR dashboard",
         title="unit drift charts",
         main=[
-            stats,
             pn.layout.Divider(margin=(20, 0, 15, 0)),
             metric_charts, 
+            pn.layout.Divider(margin=(20, 0, 15, 0)),
+            pn.Row(stats, timeline),
         ],
     )
     
