327cc31e093284624fe1ccca5962911eeffca9ed
bjhardcastle <ben.hardcastle@alleninstitute.org>
2025-02-21 14:11:40 -0800

    Run lda on ks4 units

--- scripts/run_unit_drift_lda.py
+++ scripts/run_unit_drift_lda.py
@@ -1,9 +1,14 @@
 import logging
+import pathlib
 import time
+from typing import Iterable
+
+import numpy.typing as npt
 import polars as pl
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+import tqdm
 
-logger = logging.getLogger(__file__)
+logger = logging.getLogger(pathlib.Path(__file__).stem)
 logging.basicConfig(level=logging.INFO)
 
 
@@ -82,7 +87,6 @@ df = (
 print(df.describe())
 logger.info(f"Loaded data from parquet files in {time.time() - t0:.2f} seconds")
 
-logger.info(f"Fitting LDA with leave-one-out cross-validation")
 t0 = time.time()
 METRICS_TO_KEEP = {'presence_ratio', 'vis_response_r2', 'aud_response_r2'} # , 'ancova_t_time', 'ancova_coef_time'
 COLUMNS_TO_DROP = set(df.columns) - METRICS_TO_KEEP - {'unit_id', 'drift_rating'}
@@ -101,26 +105,30 @@ annotated = (
     .drop(COLUMNS_TO_DROP)
 )
 
-def get_x_y(df: pl.DataFrame) -> tuple:
-    # z-score remaining metrics
-    df = (
-        df
-        .with_columns(
-            *[(col - col.mean()) / col.std() for col in (pl.col(name) for name in METRICS_TO_KEEP)]
-        )
+def z_score(df: pl.DataFrame, columns: Iterable[str]) -> pl.DataFrame:
+    return df.with_columns(
+        *[(col - col.mean()) / col.std() for col in (pl.col(name) for name in columns)]
     )
-    x = df.drop('unit_id', 'drift_rating', strict=False).to_numpy()
+    
+def get_x(df: pl.DataFrame) -> npt.NDArray:
+    return df.drop('unit_id', 'drift_rating', strict=False).to_numpy()
+
+def get_x_y(df: pl.DataFrame) -> tuple[npt.NDArray, npt.NDArray]:
+    # z-score remaining metrics
+    df = df.pipe(z_score, METRICS_TO_KEEP)
+    x = get_x(df)
     y = df['drift_rating'].to_numpy()
     assert x.shape[0] == y.shape[0]
     return x, y
 
 lda = LinearDiscriminantAnalysis()
 unit_id_to_value = {}
-for unit_id in annotated['unit_id']:
+
+for unit_id in tqdm.tqdm(annotated['unit_id'], total=len(annotated), unit='units', ncols=100, desc="Fitting LDA with leave-one-out cross-validation"):
     train = annotated.filter(pl.col('unit_id') != unit_id)
     test = annotated.filter(pl.col('unit_id') == unit_id)
     lda.fit(*get_x_y(train))
-    unit_id_to_value[unit_id] = lda.transform(get_x_y(test)[0]).item()
+    unit_id_to_value[unit_id] = lda.transform(get_x(test)).item()
 
 logger.info(f"Fitted LDA in {time.time() - t0:.2f} seconds")
 lda_df = (
@@ -131,8 +139,9 @@ logger.info(f"Writing LDA results for annotated units to parquet")
 lda_df.write_parquet("//allen/programs/mindscope/workgroups/dynamicrouting/ben/lda.parquet")
 print(lda_df.describe())
 
-logger.info(f"Applying LDA to all units")
+logger.info("Applying LDA score to un-annotated units")
 lda.fit(*get_x_y(annotated)) # re-fit on all annotated units
+print(dict(zip(train.drop('unit_id', 'drift_rating').columns, [float(x) for x in lda.coef_.squeeze()])))
 all_units = (
     df
     .unique('unit_id')
@@ -140,14 +149,30 @@ all_units = (
     .fill_nan(None)
     .drop_nulls(pl.all().exclude('drift_rating'))
 )
-scores = lda.transform(get_x_y(all_units)[0]).squeeze()
-assert scores.ndim == 1
-assert len(scores) == len(all_units)
-lda_df = (
-    all_units
-    .with_columns(lda=pl.Series(scores))
+ks4_units = (
+    pl.read_parquet("//allen/programs/mindscope/workgroups/dynamicrouting/ben/corr_values_ks4.parquet")
+    .join(
+        other=(
+            pl.scan_parquet("s3://aind-scratch-data/dynamic-routing/unit-rasters-ks4/units.parquet")
+            .select('unit_id', 'presence_ratio') 
+            .collect()
+        ),
+        on='unit_id',
+        how='left',
+    )
+    .fill_nan(None)
+    .drop(COLUMNS_TO_DROP, strict=False)
+    .drop_nulls()
 )
-lda_df.write_parquet("//allen/programs/mindscope/workgroups/dynamicrouting/ben/lda_all.parquet")
+for filename_suffix, units_df in {'all': all_units, 'ks4': ks4_units}.items():
+    logger.info(f"Applying LDA score to {filename_suffix} units")
+    scores = lda.transform(get_x(units_df)).squeeze()
+    assert scores.ndim == 1
+    assert len(scores) == len(units_df)
+    lda_df = (
+        units_df
+        .with_columns(lda=pl.Series(scores))
+    )
+    lda_df.write_parquet(f"//allen/programs/mindscope/workgroups/dynamicrouting/ben/lda_{filename_suffix}.parquet")
 
-print(lda_df.describe())
-print(dict(zip(train.drop('unit_id', 'drift_rating').columns, [float(x) for x in lda.coef_.squeeze()])))
+    print(lda_df.describe())
